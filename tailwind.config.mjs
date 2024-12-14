863a9affc2b5dd1c8cefb39f5823fad2b5d34ddc
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-12-14 18:18:57 -0300

    hero section completed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      dropShadow: {
+        "custom-cyan": "0 0px 3px rgba(21, 255, 254, 1)", // Ejemplo con cian
+        "custom-dark": "0 0px 3px rgba(1, 1, 1, 1)", // Ejemplo con rosado
+      },
+	  backgroundColor: {
+		"fondo": "url('/fondotv.webp)",
+	  }
+    },
+  },
+  plugins: [],
+};
